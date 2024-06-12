34c3c45d3ffe08f1a843370757df4b42886ec6b4
muthuramki <muthuramki30598@gmail.com>
2024-06-12 12:56:10 +0530

    change

--- ocr/settings.py
+++ ocr/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-oj-8uv8g=23^+orpuv-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['savicocr.azurewebsites.net', '127.0.0.1']
+ALLOWED_HOSTS = ['savicocr123.azurewebsites.net', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
